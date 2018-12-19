e9a9dbce7cf60287b8777ab04a61880cef8dab8e
Mariel Moura <mariel.moura@gmail.com>
2018-12-18 22:37:46 -0200

    backend package.json fixes

--- frontend/itlab-transaction-app/e2e/src/app.e2e-spec.ts
+++ frontend/itlab-transaction-app/e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to transaction-app!');
+    expect(page.getTitleText()).toEqual('Welcome to itlab-transaction-app!');
   });
 });
